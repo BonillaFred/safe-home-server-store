54cf8255b9eecd6e528bd802f0d1db51439b1758
Fred Bonilla <46603342+BonillaFred@users.noreply.github.com>
2022-08-20 14:33:12 -0600

    Added util class for talking to the backend
    -Needs docs 
    -Needs to be transformed into a real class 
    -Add chance to change the ports given

---
+++ fastapi-external-backend/SafeComs.py
@@ -0,0 +1,37 @@
+import requests
+
+_GET_KEY_URL = 'https://localhost:8080/publickey'
+_ENCRYPT_URL = 'https://localhost:8080/encrypt'
+_DECRYPT_URL = 'https://localhost:8080/decrypt'
+
+
+def _requestCheck(req: requests.request):
+    if req is None:
+        raise NameError("Null request: Call admin")
+    if req.status_code != 200:
+        raise NameError("Request failed with code: " + str(req.status_code))
+    return req.json()
+
+
+def _makeRequest(url: str, data: dict = None):
+    fetched = None
+    try:
+        if(data is None):
+            fetched = requests.get(url, verify='./certs/serverCert.crt')
+        else:
+            fetched = requests.post(url, json=data, verify='./certs/serverCert.crt')
+        fetched = _requestCheck(fetched)
+    except Exception as e:
+        print("Failed to get data:\n" + str(e))
+        exit(-1)
+
+    return fetched
+
+def decryptMessage(ciphertext, url=_DECRYPT_URL):
+    return _makeRequest(url, data={"message":ciphertext})
+
+def encryptMessage(plaintext, encript_url=_ENCRYPT_URL, pub_key_url=_GET_KEY_URL):
+    pubKey = _makeRequest(pub_key_url)
+    pubKey["message"] = plaintext
+    return _makeRequest(encript_url, data=pubKey)
+
