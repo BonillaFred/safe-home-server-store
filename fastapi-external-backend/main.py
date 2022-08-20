66865b82f54a82f994e4721fcce6db6062affad7
Fred Bonilla <46603342+BonillaFred@users.noreply.github.com>
2022-08-20 14:30:46 -0600

    added todo from old repo
    -dev work should continue here. 
    -TODO: has now been added.

--- fastapi-external-backend/main.py
+++ fastapi-external-backend/main.py
@@ -7,52 +7,32 @@ from jose import JWTError, jwt
 from passlib.context import CryptContext
 from pydantic import BaseModel
 
-import requests
-
-import json
-
-def requestCheck(req: requests.request):
-    if req is None:
-        raise NameError("Null request: Call admin")
-    if req.status_code != 200:
-        raise NameError("Request failed with code: " + str(req.status_code))
-    return req.json()
-
-
-def makeRequest(url: str, data: dict = None):
-    fetched = None
-    try:
-        if(data is None):
-            fetched = requests.get(url, verify='./env/certs/serverCert.crt')
-        else:
-            fetched = requests.post(url, json=data, verify='./env/certs/serverCert.crt')
-        fetched = requestCheck(fetched)
-    except Exception as e:
-        print("Failed to get data:\n" + str(e))
-        exit(-1)
-
-    return fetched
-
+# Internal module needed to simiplfy end-point code.
+import SafeComs as Coms
 
 hashHandler = CryptContext(schemes=["bcrypt"], deprecated="auto")
 oauth2_scheme = OAuth2PasswordBearer(tokenUrl="token")
 
 
-pubKey = makeRequest('https://localhost:8080/publickey')
-print(pubKey)
+# app = FastAPI()
 
-# app = FastAPI() 
+# 1. add user db( sql )
+#   1.1: User: User
+#        -path: str contains a path to the users enc files.
+#                   must never expose the top level location
+#                   of each file.
+#         -username: str
+#         -passwordHash: str
+#         -(?) public key *force them to use it.
+#
+# app = FastAPI()
+# app = FastAPI()
+
 
 def check_password(encripted_password, hashed_password):
-    encMessage = makeRequest('https://localhost:8080/decrypt', data={"message":encripted_password})
+    encMessage = Coms.decryptMessage(encripted_password)
     return hashHandler.verify(encMessage, hashed_password)
 
 
 
-
-
-pubKey["message"] = "aaaaaa"
-
-encMessage = makeRequest('https://localhost:8080/encrypt', data=pubKey)
-
-print(check_password(encMessage, hashHandler.hash("not today")))
+print(check_password(Coms.encryptMessage("hello world"), hashHandler.hash("hello world")))
