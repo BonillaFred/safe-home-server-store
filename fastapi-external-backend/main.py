9cb4f9fccee059765e8476ab601f1c676759784d
Fred Bonilla <46603342+BonillaFred@users.noreply.github.com>
2022-08-22 00:08:20 -0600

    added authenticate function
    added function that will authenticate when a user attempts to log on to the system

--- fastapi-external-backend/main.py
+++ fastapi-external-backend/main.py
@@ -1,5 +1,6 @@
 from datetime import datetime, timedelta
-from typing import Union
+from random import randint, random
+from typing import List, Text, Union
 
 from fastapi import Depends, FastAPI, HTTPException, status
 from fastapi.security import OAuth2PasswordBearer, OAuth2PasswordRequestForm
@@ -7,32 +8,52 @@ from jose import JWTError, jwt
 from passlib.context import CryptContext
 from pydantic import BaseModel
 
-# Internal module needed to simiplfy end-point code.
+# Internal module needed to simiplfy end-point code. 
 import SafeComs as Coms
+
+# Interna module needed to similify database access
+import DatabaseAccess as DbControls
+
 
 hashHandler = CryptContext(schemes=["bcrypt"], deprecated="auto")
 oauth2_scheme = OAuth2PasswordBearer(tokenUrl="token")
-
-
-# app = FastAPI()
-
-# 1. add user db( sql )
-#   1.1: User: User
-#        -path: str contains a path to the users enc files.
-#                   must never expose the top level location
-#                   of each file.
-#         -username: str
-#         -passwordHash: str
-#         -(?) public key *force them to use it.
-#
-# app = FastAPI()
-# app = FastAPI()
-
 
 def check_password(encripted_password, hashed_password):
     encMessage = Coms.decryptMessage(encripted_password)
     return hashHandler.verify(encMessage, hashed_password)
 
+'''
+input:
+    username: pgp encrypted username
+    password: pgp encrypted password
+
+output:
+    true if the user is giving us the correct information
+    false otherwise.
+
+description:
+    authenticated the current user by suppluing the pgp credentials
+'''
+def authenticate(username, password):
+    username = Coms.decryptMessage(username)
+    user = DbControls.getUser(username)
+    if user is None:
+        return False
+    return check_password(password, DbControls.getPassword(user))
 
 
-print(check_password(Coms.encryptMessage("hello world"), hashHandler.hash("hello world")))
+# DbControls.addUser("ADMIN", hashHandler.hash("hello world"))
+# DbControls.addUserUrl("ADMIN", "HELLO", "hello world")
+# print(DbControls.getUser("ADMIN"))
+# print(DbControls.getUserUrls("ADMIN", "hello world"))
+
+# print(authenticate(Coms.encryptMessage("ADMIN"), Coms.encryptMessage("hello world")))
+
+# app = FastAPI() 
+
+
+
+
+
+
+# print(check_password(Coms.encryptMessage("hello world"), hashHandler.hash("hello world")))
