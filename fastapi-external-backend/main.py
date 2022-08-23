2675c54ca7295178aa436075113e3361dd190245
Fred Bonilla <46603342+BonillaFred@users.noreply.github.com>
2022-08-23 01:36:41 -0600

    Added ability to logon without password bearer
    -> Added processes for running using https.
    -> Added process to sign and make certs when interacting with end-points
    -> Added ability to get the public key from the main server

--- fastapi-external-backend/main.py
+++ fastapi-external-backend/main.py
@@ -1,3 +1,16 @@
+'''
+MAKING HTTPS CERTS:
+openssl req -newkey rsa:4096 -new -nodes -keyout fastapi_pass.pem -out fastapi_csr.pem < make the csr
+openssl x509 -req -in fastapi_csr.pem -signkey fastapi_pass.pem -out fastapi_certi.crt -extfile serverSubjectName.ext < signs the crt
+
+** -extfile serverSubjectName.ext
+is a file that contains the following exactly:
+subjectAltName = DNS:localhost
+
+running uvicorn as https:
+uvicorn main:app --reload --ssl-keyfile './certs/fastapi_pass.pem' --ssl-certfile './certs/fastapi_certi.crt'
+'''
+
 from datetime import datetime, timedelta
 from random import randint, random
 from typing import List, Text, Union
@@ -96,9 +109,16 @@ async def get_current_user(token: str = Depends(oauth2_scheme)):
 # following from: https://fastapi.tiangolo.com/tutorial/security/oauth2-jwt/
 
 
-# DbControls.addUser("a", hashHandler.hash("a"))
-# DbControls.addUserUrl("a", "HELLO", "a")
-# print(DbControls.getUser("ADMIN"))
+# DbControls.addUser("ADMIN", hashHandler.hash("ADMIN"))
+# DbControls.addUserUrl("ADMIN", "hello world", "ADMIN")
+
+# DbControls.addUser("cat", hashHandler.hash("cat"))
+# DbControls.addUserUrl("cat", "this is a test", "cat")
+
+# DbControls.addUser("FuzzyPants", hashHandler.hash("c"))
+# DbControls.addUserUrl("FuzzyPants", "this is mr fuzzy pants", "a")
+
+# print(check_password(Coms.encryptMessage("hello world"), hashHandler.hash("hello world")))
 
 # print(authenticate(Coms.encryptMessage("ADMIN"), Coms.encryptMessage("hello world")))
 # enc = Coms.encryptMessage("hello world")
@@ -109,7 +129,29 @@ app = FastAPI()
 
 @app.post("/token", response_model=Token)
 async def login_for_access_token(form_data: OAuth2PasswordRequestForm = Depends()):
-    user = authenticate(form_data.username, form_data.password)
+    username = Coms.encryptMessage(form_data.username)
+    password = Coms.encryptMessage(form_data.password)
+
+    user = authenticate(Coms.decryptMessage(username), Coms.decryptMessage(password))
+    if not user:
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail="Incorrect username or password",
+            headers={"WWW-Authenticate": "Bearer"},
+        )
+    access_token_expires = timedelta(minutes=env["ACCESS_TOKEN_EXPIRE_MINUTES"])
+    access_token = create_access_token(
+        data={"sub": str(user.username)[2:]}, expires_delta=access_token_expires
+    )
+    return {"access_token": access_token, "token_type": "bearer"}
+
+
+@app.post("/logon", response_model=Token)
+async def login_for_access_token(form_data: User):
+    username = Coms.encryptMessage(form_data.username)
+    password = Coms.encryptMessage(form_data.password)
+
+    user = authenticate(Coms.decryptMessage(username), Coms.decryptMessage(password))
     if not user:
         raise HTTPException(
             status_code=status.HTTP_401_UNAUTHORIZED,
@@ -126,17 +168,30 @@ async def login_for_access_token(form_data: OAuth2PasswordRequestForm = Depends(
 @app.post("/users/me/resources")
 async def read_users_me(data: URLGET, current_user: User = Depends(get_current_user)):
     user = DbControls.getUserEncrypted(str(current_user.username)[2:])
-    #needs encryption here
-    lists = DbControls.getUserUrls(user.username, data.password)
+
+    #Remove the following when the front-end is done
+    #password = Coms.encryptMessage(data.password)
+
+    password = Coms.decryptMessage(data.password)
+
+    lists = DbControls.getUserUrls(user.username, password)
     return {"urls": lists}
 
 @app.post("/users/me/addresource")
 async def read_users_me(data: URLADD, current_user: User = Depends(get_current_user)):
     user = DbControls.getUserEncrypted(str(current_user.username)[2:])
     #needs encryption here
-    res = DbControls.addUserUrl(user.username, data.url, data.password)
+    
+    #Remove these when the front-end is done being implemented
+    # password = Coms.encryptMessage(data.password)
+    # url = Coms.encryptMessage(data.url)
+
+    password = Coms.decryptMessage(password)
+    url = Coms.decryptMessage(url)
+
+    res = DbControls.addUserUrl(user.username, url, password)
     return {"added": res}
 
-
-
-# print(check_password(Coms.encryptMessage("hello world"), hashHandler.hash("hello world")))
+@app.get('/handshake')
+async def getPublicKey():
+    return Coms.getPublicKey()
